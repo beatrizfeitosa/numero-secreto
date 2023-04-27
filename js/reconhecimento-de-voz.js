58516dbfec715485ffa19f3171063f1a97c5db82
Beatriz Feitosa <71518594+beatrizfeitosa@users.noreply.github.com>
2023-04-27 14:50:58 -0300

    Add files via upload

---
+++ js/reconhecimento-de-voz.js
@@ -0,0 +1,26 @@
+window.SpeechRecognition = window.SpeechRecognition || webkitSpeechRecognition;
+
+const recognition = new SpeechRecognition();
+recognition.lang = 'pt-Br';
+
+recognition.start();
+
+recognition.addEventListener('result', onSpeak);
+
+const elementoChute = document.getElementById('chute');
+
+function onSpeak(e) {
+    chute = e.results[0][0].transcript;
+    exibeChuteNaTela(chute);
+    verificaSeOChutePossuiUmValorValido(chute);
+}
+
+function exibeChuteNaTela(chute) {
+    elementoChute.innerHTML = `
+        <div>Você disse:</div>
+        <span class="box">${chute}</span>
+    `
+}
+
+//permite continuar a falar sucessivamente
+recognition.addEventListener('end', () => recognition.start());
---
+++ js/validacao.js
@@ -0,0 +1,43 @@
+function verificaSeOChutePossuiUmValorValido(chute) {
+    const numero = +chute; //numero inteiro
+
+    if (seChuteForInvalido(numero)) {
+        elementoChute.innerHTML += '<div>Valor inválido</div>';
+        return;
+    }
+
+    if (numeroForMaiorOuMenorQueOPermitido(numero)) {
+        elementoChute.innerHTML +=  `<div>Valor inválido: fale um número entre ${menorValor} e ${maiorValor}</div>`;
+        return;
+    } else if (numero > numeroSecreto) {
+        elementoChute.innerHTML += `
+            <div>O número secreto é menor <i class="fa-solid fa-arrow-down"></i></div>
+        `;
+    } else {
+        elementoChute.innerHTML += `
+            <div>O número secreto é maior <i class="fa-solid fa-arrow-up"></i></div>
+        `;
+    }
+
+    if (numero === numeroSecreto) {
+        document.body.innerHTML = `
+            <h1>Você acertou!</h1>
+            <h3>O número secreto era ${numeroSecreto}</h3>
+            <button id="jogarNovamente" class="btn-jogar">Jogar novamente</button>
+        `;
+    }
+}
+
+function seChuteForInvalido(numero) {
+    return Number.isNaN(numero);
+}
+
+function numeroForMaiorOuMenorQueOPermitido(numero) {
+    return numero > maiorValor || numero < menorValor;
+}
+
+document.body.addEventListener('click', e => {
+    if (e.target.id == 'jogarNovamente') {
+        window.location.reload();
+    }
+})
